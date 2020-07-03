8d3ad708ac6520126063c6292b1fd95d66266dc6
Manny <manny@His-Mac.local>
2020-07-03 15:07:14 -0400

    FIX: clean up of unused commented code

--- packages/web/index.js
+++ packages/web/index.js
@@ -4,6 +4,4 @@ const PORT = process.env.PORT || 2000;
 
 app.get("/", express.static("static"));
 
-// app.get("/", (req, res) => res.send({ version: VERSION }));
-
 app.listen(PORT, () => console.log(`Website listening on ${PORT}`));
